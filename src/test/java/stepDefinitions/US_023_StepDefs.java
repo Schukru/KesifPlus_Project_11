03252d163d180c298ea336a455399265456df58a
Schukru <kilicsukru@hotmail.com>
2024-05-08 00:00:04 +0300

    US_023 Login with Google test edildi, Utilities class'ına yeni reusable metodlar eklendi.

---
+++ src/test/java/stepDefinitions/US_023_StepDefs.java
@@ -0,0 +1,23 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.CommonPage;
+
+public class US_023_StepDefs extends CommonPage {
+    @Given("User clicks login with google button")
+    public void userClicksLoginWithGoogleButton() {
+         getHomePage().clickGoogleLoginButton();
+    }
+
+    @And("User account information should be entered")
+    public void userAccountInformationShouldBeEntered() {
+        getHomePage().inputGmailAccount();
+    }
+
+    @Then("Verify that the project page can be accessible")
+    public void verifyThatTheProjectPageCanBeAccessible() {
+        getHomePage().verifyProjectPage();
+    }
+}
